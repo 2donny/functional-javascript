bbb4f5c207e4d5a4f41c64864337fe21c43edac1
EdonJung <2donny@naver.com>
2021-08-15 17:43:50 +0900

    01/1 es6

--- 01. 이터러블 프로그래밍 혹은 리스트 프로세싱/1.js
+++ 01. 이터러블 프로그래밍 혹은 리스트 프로세싱/1.js
@@ -2,7 +2,7 @@
 
 // 이터러블 프로그래밍 혹은 리스트 프로세싱 (Lisp)
 
-// 1. 홀수 n개 더하기
+// 1. 홀수 n개 더하기 (Imperative programming)
 
 function f1(limit, list) {
     let acc = 0;
@@ -12,13 +12,23 @@ function f1(limit, list) {
             acc += b;
             if (--limit == 0) break;
         }
-    }   
+    }
     console.log(acc);
 }
-f1(3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-// 2. if를 filter로
+// Iterable programming
+// 2. if가 filter로 추상화된다.
 // 3. 값 변화 후 변수 할당을 map으로
 // 4. break를 take로
 // 5. 축약 및 합산을 reduce로
 
+function f2(limit, list) {
+    _.go(list,
+        L.filter(a => a % 2),
+        L.map(a => a * a),
+        L.take(limit),
+        _.reduce(add),
+        console.log);
+}
+f2(3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.clear();
